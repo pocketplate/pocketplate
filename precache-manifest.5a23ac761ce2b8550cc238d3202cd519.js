f7c423de9e22bb6022c4f721f7f23fe253c375b3
Alexandre Consigny <consigal@r3d.com>
2019-07-19 19:01:11 -0400

    Updates

---
+++ precache-manifest.5a23ac761ce2b8550cc238d3202cd519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b712f790c3a819e646e9cf9eb0bb2771",
+    "url": "/pocketplate/index.html"
+  },
+  {
+    "revision": "3b605148a487426c8cc6",
+    "url": "/pocketplate/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pocketplate/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3b605148a487426c8cc6",
+    "url": "/pocketplate/static/js/main.e625952d.chunk.js"
+  },
+  {
+    "revision": "72fd7ae3158158b56558",
+    "url": "/pocketplate/static/js/runtime~main.955d80de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pocketplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
