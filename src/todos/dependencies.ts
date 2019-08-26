3237e8f751eceb0882efc618416df75ffac4c2fb
alexandre.consigny <consigal@r3d.com>
2019-08-26 11:40:05 -0400

    Cleanedup project

--- src/todos/dependencies.ts
+++
@@ -1,6 +0,0 @@
-import { container } from '../ioc';
-import { TodosService } from './services/TodosService';
-import { TodoStore } from './stores/TodoStore';
-
-container.bind<TodosService>(TodosService).toSelf().inSingletonScope();
-container.bind<TodoStore>(TodoStore).toSelf().inSingletonScope();
