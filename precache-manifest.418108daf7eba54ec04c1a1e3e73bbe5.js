9c5f69ca3cdd65f7786422c924f7c1a68d6e8273
pocketplate <alexandre.consigny+2@r3d.com>
2019-08-26 15:54:52 +0000

    Updates

---
+++ precache-manifest.418108daf7eba54ec04c1a1e3e73bbe5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4728230ca7dd3f3b686277bb8b2ae868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0992b7e6ff7b3ca3c73c",
+    "url": "/static/css/2.85af740b.chunk.css"
+  },
+  {
+    "revision": "70e3feaf8058b20d835e",
+    "url": "/static/css/4.6bfb0f0b.chunk.css"
+  },
+  {
+    "revision": "8d2355a4042c3ef80b91",
+    "url": "/static/css/main.626cbd66.chunk.css"
+  },
+  {
+    "revision": "0992b7e6ff7b3ca3c73c",
+    "url": "/static/js/2.0d38718f.chunk.js"
+  },
+  {
+    "revision": "ec9fe0d4dea3fe86baf2",
+    "url": "/static/js/3.064a932a.chunk.js"
+  },
+  {
+    "revision": "70e3feaf8058b20d835e",
+    "url": "/static/js/4.45f8747f.chunk.js"
+  },
+  {
+    "revision": "8d2355a4042c3ef80b91",
+    "url": "/static/js/main.888f3c31.chunk.js"
+  },
+  {
+    "revision": "ad992e5e4a1a99103d83",
+    "url": "/static/js/runtime~main.209d7c90.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pocketplate/precache-manifest.f99eb0ab1e09f40406fb42c7a1601d99.js"
+  "/precache-manifest.418108daf7eba54ec04c1a1e3e73bbe5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pocketplate/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
