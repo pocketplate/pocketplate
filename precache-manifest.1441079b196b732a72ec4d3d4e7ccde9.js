27825a01127065c339ee349c589a6ab275ec2026
alexandre.consigny <consigal@r3d.com>
2019-08-26 15:26:25 -0400

    Updates

---
+++ precache-manifest.1441079b196b732a72ec4d3d4e7ccde9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f8ded938c8d26f117b9c22860f2461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b1107221119e259d912",
+    "url": "/static/css/2.9a87060b.chunk.css"
+  },
+  {
+    "revision": "b6f9452cba0fa95cef18",
+    "url": "/static/css/4.dc4d8cc4.chunk.css"
+  },
+  {
+    "revision": "306a867c7894b4fff71d",
+    "url": "/static/css/main.0650e84e.chunk.css"
+  },
+  {
+    "revision": "1b1107221119e259d912",
+    "url": "/static/js/2.0d38718f.chunk.js"
+  },
+  {
+    "revision": "bbc8842fd6dd6fa7c83d",
+    "url": "/static/js/3.e98dcec7.chunk.js"
+  },
+  {
+    "revision": "b6f9452cba0fa95cef18",
+    "url": "/static/js/4.ccb26e9f.chunk.js"
+  },
+  {
+    "revision": "306a867c7894b4fff71d",
+    "url": "/static/js/main.3b2ae86f.chunk.js"
+  },
+  {
+    "revision": "a2bff2fae60440349252",
+    "url": "/static/js/runtime~main.37dbb582.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.418108daf7eba54ec04c1a1e3e73bbe5.js"
+  "/precache-manifest.1441079b196b732a72ec4d3d4e7ccde9.js"
 );
 
 self.addEventListener('message', (event) => {
