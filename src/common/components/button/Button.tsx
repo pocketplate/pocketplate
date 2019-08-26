a0caf707e0b075d5679785c8a95ff89feb42a558
alexandre.consigny <consigal@r3d.com>
2019-08-26 11:38:09 -0400

    Cleanup project

--- src/common/components/button/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from 'react-bootstrap';
-import './Button.scss';
-import * as React from 'react';
-
-export const AmpButton = (props) => <Button {...props} />;
--- src/pound-calculator/stores/BarStore.spec.ts
+++ src/pound-calculator/stores/BarStore.spec.ts
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 describe('BarStore', () => {
   it('', () => {
     expect(1).toBe(1);
